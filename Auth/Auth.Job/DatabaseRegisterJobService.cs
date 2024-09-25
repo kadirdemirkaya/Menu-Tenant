7f24a52cda59d4e6bb6975a20b5d17635667dab3
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2024-09-25 11:47:16 +0300

    auth job service app added

---
+++ Auth/Auth.Job/DatabaseRegisterJobService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using SecretManagement;
+using Shared.Application.Abstractions;
+using Shared.Domain.Aggregates.UserAggregate.Entities;
+using Shared.Domain.Aggregates.UserAggregate.ValueObjects;
+using Shared.Infrastructure.Extensions;
+
+namespace Auth.Job
+{
+    public class DatabaseRegisterJobService(IRepository<ConnectionPool, ConnectionPoolId> _repository, ISecretsManagerService secretsManagerService, ILogger<DatabaseRegisterJobService> _logger) : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                List<ConnectionPool>? connectionPools = await _repository.GetAllAsync(c => c.IsActive == false || c.DatabaseName.StartsWith("personaldb_"), false, true, c => c.Company, com => com.Company.AppUser);
+
+                if (connectionPools.Count() != 0)
+                {
+                    connectionPools.ForEach(async con =>
+                    {
+                        if (con.IsActive == false && con.DatabaseName.StartsWith("personaldb_"))
+                        {
+                            string personalDbName = $"{con.Company.AppUser.Username}{StringExtension.GenerateTemporaryDatabaseName()}db";
+
+                            con.SetDatabaseName(personalDbName);
+                            con.SetUpdatedDateUTC(DateTime.UtcNow);
+
+                            bool isUpdate = false;
+
+                            isUpdate = _repository.Update(con);
+                            isUpdate = await _repository.SaveCahangesAsync();
+
+                            if (isUpdate)
+                                _logger.LogError("got error in system while table udpate");
+
+                            if (isUpdate)
+                            {
+                                //
+                                // in this section will send event for db create process
+                                //
+                            }
+
+                        }
+                        else
+                        {
+                            string personalDbName = con.DatabaseName;
+
+                            con.SetUpdatedDateUTC(DateTime.UtcNow);
+
+                            //
+                            // in this section will send event for db create process
+                            //
+                        }
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Auth/Auth.Job/Program.cs
@@ -0,0 +1,62 @@
+﻿using Auth.Job;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        IConfiguration configuration;
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        configuration = builder.Build();
+
+        var sp = services.BuildServiceProvider();
+
+        #region Unnecassery
+        //using (ISecretsManagerService sManagerService = new AwsSecretsManagerService(configuration))
+        //{
+        //    services.AddDbContext<AuthDbContext>(options =>
+        //    {
+        //        options.UseNpgsql("Server=localhost;port=5432;Database=authdb;User Id=admin;Password=passw00rd");
+        //        options.UseNpgsql($"Server={Constants.Secrets.DevelopmentPOSTGRES_Host};port={Constants.Secrets.DevelopmentPOSTGRES_Port};Database={Constants.Secrets.DevelopmentPOSTGRES_POSTGRES_AuthDb};User Id={Constants.Secrets.DevelopmentPOSTGRES_POSTGRES_User};Password={Constants.Secrets.DevelopmentPOSTGRES_POSTGRES_Password}");
+        //    });
+        //}
+
+        //services.AddScoped<IRepository<Company, CompanyId>, Repository<Company, CompanyId>>();
+
+        //services.AddScoped<IRepository<ConnectionPool, ConnectionPoolId>, Repository<ConnectionPool, ConnectionPoolId>>();
+
+        //services.SeqRegistration(configuration);
+
+        //services.SecretManagementRegistration();
+        #endregion
+        services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+
+        services.AddQuartz(configurator =>
+        {
+            configurator.UseMicrosoftDependencyInjectionJobFactory();
+
+            JobKey jobKey = new("Auth.Job.Service.Key");
+
+            configurator.AddJob<DatabaseRegisterJobService>(options => options.WithIdentity(jobKey));
+
+            TriggerKey triggerKey = new("Auth.Job.Service.Trigger.Key");
+
+            configurator.AddTrigger(options => options.ForJob(jobKey)
+                        .WithIdentity(triggerKey)
+                        .StartAt(DateTime.UtcNow)
+                        .WithSimpleSchedule
+                        (
+                            builder => builder.WithIntervalInSeconds(30)
+                                              .RepeatForever()
+                        ));
+        });
+    })
+    .Build();
+
+await host.RunAsync();
